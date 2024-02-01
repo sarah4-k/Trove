1180318effbb6f1d636ea6c5a1470d41a8bea12f
Aryaf272 <148352612+Aryaf272@users.noreply.github.com>
2024-02-01 15:28:06 +0300

    Update NotificationService.swift

--- ios/ImageNotification/NotificationService.swift
+++ ios/ImageNotification/NotificationService.swift
@@ -19,7 +19,7 @@ class NotificationService: UNNotificationServiceExtension {
     
     override func serviceExtensionTimeWillExpire() {
         // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        // Use this as an opportunity to deliver your "best attempt" at modified content
         if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
             contentHandler(bestAttemptContent)
         }
